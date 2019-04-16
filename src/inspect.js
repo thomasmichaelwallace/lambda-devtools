a25b176cf39a65061ddf791b583684b41356e0ee
thomasmichaelwallace <thomasmichaelwallace@gmail.com>
2019-04-16 17:29:37 +0100

    feat: jsdocs for public api

--- src/inspect.js
+++ src/inspect.js
@@ -7,6 +7,21 @@ const logger = require('./utilities/logger')('inspect');
 let inspectorUrl;
 let bridge;
 
+/**
+ * Inspector for remote DevTools
+ * @param {Object} options lambda-devtools configuration
+ * @param {boolean} [options.enabled=true] if true (default) inspector will be enabled and attached
+ * @param {boolean} [options.patchConsole=true] if true (default) console[debug|info|log|warn|error]
+ * @param {integer} [options.port=9229] port (9229 by default) to use for node inspector
+ *  will be (re-)patched to report in devtools
+ * @param {Object} options.iot aws-iot-device-sdk-js awsIot.device options
+ *  see https://github.com/aws/aws-iot-device-sdk-js#device for full description.
+ *  Note that clientId will be automatically assigned.
+ * @param {string} options.iot.host AWS IoT endpoint to use for broker
+ * @param {string} options.iot.certPath path to AWS IoT issued client certificate
+ * @param {string} options.iot.keyPath path to AWS IoT issued private key
+ * @param {string} options.iot.caPath path to AWS IoT issued CA certificate file
+ */
 function inspect(options) {
   const { enabled = true, patchConsole = true, iot } = options;
   if (!enabled) {
