5ee39ee72aa7d732c47207511ff79eb5d7fb6ffa
thomasmichaelwallace <thomasmichaelwallace@gmail.com>
2019-04-16 19:40:43 +0100

    refactor: clarify bridge public interface

--- src/bridges/Iot.js
+++ src/bridges/Iot.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 const awsIot = require('aws-iot-device-sdk');
 const tryParse = require('../utilities/tryParse');
 const logger = require('../utilities/logger')('bridges:iot');
@@ -6,20 +7,21 @@ const ANNOUNCE_TOPIC = 'lambda-devtools/announce';
 const LAMBDA_TOPIC_PREFIX = 'lambda-devtools/lambda';
 const DEVTOOLS_TOPIC_PREFIX = 'lambda-devtools/devtools';
 
+let iot;
+
 class Iot {
   constructor({
-    iot, iotConfig, onMessage, topics, onReady = () => {},
+    iotConfig, onMessage, topics, onReady = () => {},
   }) {
     this.emitMessage = onMessage;
     this.topics = topics;
     if (iot) {
-      this.iot = iot;
       this.onConnect();
       onReady();
     } else {
       logger.debug({ iotConfig }, 'connecting to aws iot');
-      this.iot = awsIot.device(iotConfig);
-      this.iot.on('connect', () => {
+      iot = awsIot.device(iotConfig);
+      iot.on('connect', () => {
         logger.debug({ clientId: iotConfig.clientId }, 'connected');
         this.onConnect();
         onReady();
@@ -31,8 +33,8 @@ class Iot {
     if (!this.topics.inbound) {
       return;
     }
-    this.iot.subscribe(this.topics.inbound);
-    this.iot.on('message', (...args) => this.onMessage(...args));
+    iot.subscribe(this.topics.inbound);
+    iot.on('message', (...args) => this.onMessage(...args));
     logger.debug({ topic: this.inbound }, 'subscribed');
   }
 
@@ -48,70 +50,86 @@ class Iot {
       logger.warn({ message, topics: this.topics }, 'cannot send message without outbound topic');
       return;
     }
-    this.iot.publish(this.topics.outbound, message);
+    iot.publish(this.topics.outbound, message);
   }
 }
 
-class IotLambdaBridge extends Iot {
+class IotLambdaBridge {
   constructor(session, onMessage, iotConfig) {
     const topics = {
       inbound: `${LAMBDA_TOPIC_PREFIX}/${session.id}`,
       outbound: `${DEVTOOLS_TOPIC_PREFIX}/${session.id}`,
     };
     const clientId = `lambda-${session.id}`;
-    super({
+    this._iot = new Iot({
       iotConfig: { clientId, ...iotConfig },
       onMessage,
-      onReady: () => this.onReady(),
+      onReady: () => this._onReady(),
       topics,
     });
-    this.session = session;
+    this._session = session;
   }
 
-  onReady() {
-    logger.debug({ session: this.session }, 'announced');
-    this.iot.publish(ANNOUNCE_TOPIC, JSON.stringify(this.session));
+  // public
+  send(message) {
+    this._iot.send(message);
+  }
+
+  // "private"
+  _onReady() {
+    logger.debug({ session: this._session }, 'announced');
+    iot.publish(ANNOUNCE_TOPIC, JSON.stringify(this._session));
   }
 }
 
-class IotDevtoolsBridge extends Iot {
-  constructor(id, onMessage, iot) {
+class IotDevtoolsBridge {
+  constructor(id, onMessage) {
     const topics = {
       inbound: `${DEVTOOLS_TOPIC_PREFIX}/${id}`,
       outbound: `${LAMBDA_TOPIC_PREFIX}/${id}`,
     };
-    super({ onMessage, topics, iot });
+    this._iot = new Iot({ onMessage, topics });
+  }
+
+  send(message) {
+    this._iot.send(message);
   }
 }
 
-class IotClientBridge extends Iot {
+class IotClientBridge {
   constructor(iotConfig) {
     const uid = Math.random().toString(36).replace('.', '');
     const clientId = `lambda-devtools-${uid}`;
     const topics = { inbound: ANNOUNCE_TOPIC };
-    const onMessage = message => this.onAnnounce(tryParse(message));
-    super({ iotConfig: { clientId, ...iotConfig }, onMessage, topics });
+    const onMessage = message => this._onAnnounce(tryParse(message));
+    this._iot = new Iot({ iotConfig: { clientId, ...iotConfig }, onMessage, topics });
 
-    this.connections = {};
-    this.sessions = {};
+    this._connections = {};
+    this._sessions = {};
   }
 
+  // public
   connect(id, onMessage) {
-    if (this.connections[id]) {
-      return this.connections[id];
+    if (this._connections[id]) {
+      return this._connections[id];
     }
-    const devtools = new IotDevtoolsBridge(id, onMessage, this.iot);
-    this.connections[id] = devtools;
-    return this.connections[id];
+    const devtools = new IotDevtoolsBridge(id, onMessage);
+    this._connections[id] = devtools;
+    return this._connections[id];
   }
 
-  onAnnounce(session) {
+  get sessions() {
+    return this._sessions;
+  }
+
+  // "private"
+  _onAnnounce(session) {
     if (!session || !session.id) {
       logger.warn({ session }, 'invalid session announced');
       return;
     }
     logger.info({ session }, 'new session announced');
-    this.sessions[session.id] = session;
+    this._sessions[session.id] = session;
   }
 }
 
