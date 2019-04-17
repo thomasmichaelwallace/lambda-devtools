5a1ebeb092bf05b32f69acee83be7459067ee1ff
thomasmichaelwallace <thomasmichaelwallace@gmail.com>
2019-04-17 14:58:39 +0100

    fix: prevent null cw logs on patched console

--- src/patches/console.js
+++ src/patches/console.js
@@ -1,28 +1,39 @@
+const stream = require('stream');
 const tryParse = require('../utilities/tryParse');
 const logger = require('../utilities/logger')('patches:console');
 
 const PATCHED_ARG_A = null;
 const PATCHED_ARG_B = '__lambda_devtools';
 
+const nullStream = new stream.Writable();
+/* eslint-disable no-console, no-underscore-dangle */
+const consoleStdout = console._stdout;
+const consoleStderr = console._stderr;
+/* eslint-enable */
+
 function patch() {
-  /* eslint-disable no-console */
+  /* eslint-disable no-console, no-underscore-dangle */
   ['debug', 'info', 'log', 'warn', 'error'].forEach((key) => {
     const type = key === 'warn' ? 'warning' : key;
     if (console[key].name === 'bound consoleCall') {
       logger.debug({ key }, 'left un-patched console');
       return;
     }
-    if (console[key].__lambda_devtools_patched === true) { // eslint-disable-line no-underscore-dangle, max-len
+    if (console[key][PATCHED_ARG_B] === true) {
       logger.debug({ key }, 'skipping patched console');
       return;
     }
     logger.info({ key }, 'patched console');
     const { [key]: forward } = console;
     console[key] = (...args) => {
+      console._stdout = nullStream;
+      console._stderr = nullStream;
       console.dir(PATCHED_ARG_A, PATCHED_ARG_B, type, ...args);
+      console._stdout = consoleStdout;
+      console._stderr = consoleStderr;
       forward(...args);
     };
-    console[key].__lambda_devtools_patched = true; // eslint-disable-line no-underscore-dangle
+    console[key][PATCHED_ARG_B] = true;
   });
   /* eslint-enable */
 }
